f9660416fa95dc2280258352c14cee38d17f3d85
Mark Ulrich <mark.ulrich.86@gmail.com>
2019-05-12 21:25:40 -0500

    Submitted on codepen

--- scripts/main.js
+++ scripts/main.js
@@ -9,13 +9,9 @@ const drawChart = (data) => {
   const chartWidth = 800;
   const chartHeight = 500;
   const chartPadding = 50;
+  const titleX = 250;
+  const titleY = 50;
   const barWidth = 2.2;
-
-  // Title
-  d3.select('#chart-container')
-    .append('h1')
-    .attr('id', 'title')
-    .text(ChartTitle);
 
   // Create the chart svg
   const svg = d3
@@ -23,8 +19,15 @@ const drawChart = (data) => {
     .append('svg')
     .attr('width', chartWidth)
     .attr('height', chartHeight)
-    .attr('class', 'chart')
-    .style('box-shadow', '1px 1px 1px 1px #222222');
+    .attr('class', 'chart');
+
+  // Title
+  svg
+    .append('text')
+    .attr('id', 'title')
+    .attr('x', titleX)
+    .attr('y', titleY)
+    .text(ChartTitle);
 
   // Axes
   const quarters = data.map((data) => {
@@ -80,6 +83,17 @@ const drawChart = (data) => {
     .attr('transform', `translate(${chartPadding}, ${chartPadding})`)
     .call(yAxis);
 
+  // Label
+  const yLabelX = 80;
+  const yLabelY = 350;
+  const yLabel = svg
+    .append('text')
+    .attr('id', 'y-label')
+    .attr('x', yLabelX)
+    .attr('y', yLabelY)
+    .attr('transform', `rotate(270, ${yLabelX}, ${yLabelY})`)
+    .text('Gross Domestic Product');
+
   // Draw bars
   svg
     .selectAll('rect')
@@ -105,7 +119,7 @@ const drawChart = (data) => {
       const gdp = e.target.getAttribute('data-gdp');
       drawTooltip(e.clientX, date, gdp);
     });
-    bar.addEventListener('mouseout', (e) => {
+    bar.addEventListener('mouseleave', (e) => {
       const tooltip = document.getElementById('tooltip');
       if (tooltip) tooltip.style.visibility = 'hidden';
     });
@@ -113,14 +127,12 @@ const drawChart = (data) => {
 };
 
 const drawTooltip = (mouseX, date, gdp) => {
-  const offsetX = 50;
-  const positionY = 300;
+  const offsetX = 30;
+  const positionY = 400;
 
   const tooltip = document.getElementById('tooltip');
-
-  tooltip.style.top = positionY;
-  // tooltip.style.left = mouseX + offsetX;
-  tooltip.style.left = 100;
+  tooltip.style.top = `${positionY}px`;
+  tooltip.style.left = `${mouseX + offsetX}px`;
   tooltip.style.visibility = 'visible';
   tooltip.setAttribute('data-date', date);
 
@@ -143,8 +155,8 @@ const drawTooltip = (mouseX, date, gdp) => {
         console.error('Bad Value');
     }
   })();
-  const text = `${year} Q${quarter}<br>$${gdp} Billion`;
-  tooltip.innerHTML = text;
+  const markup = `<strong>${year} Q${quarter}</strong><br>$${gdp} Billion`;
+  tooltip.innerHTML = markup;
 };
 
 const getData = () => {
